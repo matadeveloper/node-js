2c2dbeb7641b634f819db97e67330a0a9f3266de
DESKTOP-LMKOB8A\user <kharismamaulana1@gmail.com>
2022-06-07 19:49:10 +0700

    add config for env development

--- server.js
+++ server.js
@@ -6,12 +6,21 @@
 */
 require('module-alias/register')
 const cluster = require('cluster')
+const express = require('express')
 const os = require('os')
 const http = require('http')
 const {app} = require('./app')
 const {Route} = require('./core/route')
 
+const errorHandler = (error, request, response, next) => {
+    // Error handling middleware functionality
+    console.log( `error ${error.message}`) // log the error
+    const status = error.status || 400
+    // send back an easily understandable error message to the caller
+    response.status(status).send(error.message)
+  }
 class App extends Route{
+    
     init(){
         if (cluster.isMaster) {
             let cpuCore = os.cpus().length
@@ -27,8 +36,15 @@ class App extends Route{
                 cluster.fork()
             })
         } else {
+
+            var stateDevelopment = ['production','staging'];
             //init default route
             app.use(super.init())
+            if(stateDevelopment.indexOf(process.env.NODE_ENV) >= 0){
+                // app.use(errorHandler);
+            }else{
+                app.use(errorHandler);
+            }
 
             app.engine('html', require('ejs').renderFile);
             app.set('views', __dirname + '/views');
